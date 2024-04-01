8146d27bada5274c9a1e5d05c0f0b5f7ec02baec
Abhishek Kumar <136926247+im-DataEngineer@users.noreply.github.com>
2024-04-01 14:03:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# def factorial(n):
+#     """
+#     Compute the factorial of a non-negative integer n.
+#
+#     Args:
+#     - n: An integer
+#
+#     Returns:
+#     - factorial: The factorial of n
+#     """
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# def reverse_string(input_string):
+#     return input_string[::-1]
+
+#
+# def nested_function(a, b):
+#     return a * b
+#
+# def parent_function(x, y):
+#     result = nested_function(x, y)
+#     return result
+
+if __name__ == "__main__":
+    print(add_numbers(5,6))
+
+
